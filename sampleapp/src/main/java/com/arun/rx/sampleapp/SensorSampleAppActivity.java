8189404ea2a6d9237a1138d9bf96dc87029f04f3
arun <arun.s@allegion.com>
2018-10-30 11:07:59 +0530

    Renamed activity name

--- sampleapp/src/main/java/com/arun/rx/sampleapp/SensorSampleAppActivity.java
+++ sampleapp/src/main/java/com/arun/rx/sampleapp/SensorSampleAppActivity.java
@@ -1,13 +1,13 @@
 package com.arun.rx.sampleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SensorSampleAppActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sensor_sample_app);
     }
 }
